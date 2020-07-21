524893597c7c574a4dc47a54d1b61bd1e58aca1d
kimkihoon <63439911+kimkihoon0515@users.noreply.github.com>
2020-07-21 20:25:09 +0900

    2차과제 업로드입니다

---
+++ unit-test-2week/src/main/java/domain/Champion.java
@@ -0,0 +1,13 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Champion {
+    private String name;
+    private String position;
+    private int hasSkinCount;
+
+}
